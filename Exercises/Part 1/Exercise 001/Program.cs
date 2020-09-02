99a71bff1cf21489b5615d998c50f0cc7a712a8d
Tero Niemi <tero.niemi@centria.fi>
2020-09-02 12:01:06 +0300

    2.9.2020

--- Exercises/Part 1/Exercise 001/Program.cs
+++ Exercises/Part 1/Exercise 001/Program.cs
@@ -10,7 +10,7 @@ namespace Exercise_001
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world 1!");
+            Console.WriteLine("Hello world!");
             Console.ReadLine();
         }
     }
