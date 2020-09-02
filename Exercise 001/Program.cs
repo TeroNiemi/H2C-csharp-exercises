ad83a328d4c53578e14c95848184c5155c6c0f79
Tero Niemi <tero.niemi@centria.fi>
2020-09-02 10:12:33 +0300

    2.9.2020

---
+++ Exercise 001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world 1!");
+            Console.ReadLine();
+        }
+    }
+}
