4ca8631586534ce7bdb06a502ddaef0f6547e6fb
Tero Niemi <tero.niemi@centria.fi>
2020-09-11 13:19:02 +0300

    NOT DONE

--- Exercises/Part 1/Exercise 001/Program.cs
+++ Exercises/Part 1/Exercise 001/Program.cs
@@ -6,28 +6,9 @@ namespace exercise_01
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // your code here
            
         }
-         public void TestExercise01()
-        {
-            using (StringWriter sw = new StringWriter())
-            {
-                // Save a reference to the standard output.
-                TextWriter stdout = Console.Out;
-
-                // Redirect standard output to variable.
-                Console.SetOut(sw);
-
-                // Call student's code
-                Program.Main(null);
-
-                // Restore the original standard output.
-                Console.SetOut(stdout);
-
-                // Assert
-                Assert.AreEqual( "Hello World!\n", sw.ToString().Replace("\r\n","\n"), "Check your code! The test requires exact match!");
-            }
-        }
+        
     }
 }
