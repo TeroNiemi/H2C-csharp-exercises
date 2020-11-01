838bd893115c6946a115c6616a4497a9efbeb8c5
Ada Udoji <ada.udoji@centria.fi>
2020-11-02 01:25:55 +0200

    2.11.2020

--- Exercises/Part 1/Exercise 003/Program.cs
+++ Exercises/Part 1/Exercise 003/Program.cs
@@ -6,7 +6,11 @@ namespace exercise_03
     {
         public static void Main(string[] args)
         {
-            // Your code here:
+            Console.WriteLine("Once upon a time\n" +
+                "I was falling in love\n" +
+                 "Now I'm only falling apart");
+            Console.ReadKey();
+
         }
     }
 }
