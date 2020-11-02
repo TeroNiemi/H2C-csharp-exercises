f9eb473a97acd417e14c65524ebe5f164d25aae9
Ada Udoji <ada.udoji@centria.fi>
2020-11-02 02:53:37 +0200

    2.11.2020

--- Exercises/Part 1/Exercise 007/Program.cs
+++ Exercises/Part 1/Exercise 007/Program.cs
@@ -6,8 +6,12 @@ namespace exercise_07
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Give input!");
-            // Write your code here:
+            Console.Write("Terve in English is: ");
+            String name = Console.ReadLine();
+          
+            Console.WriteLine("Hello\nHello\nHello");
+
+            Console.ReadLine();
             
         }
     }
