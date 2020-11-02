c9e908dfc9ca96d6aa167f7935490d362fa206fa
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-02 17:15:32 +0200

    2.11.2020

--- Exercises/Part 1/Exercise 008/Program.cs
+++ Exercises/Part 1/Exercise 008/Program.cs
@@ -6,8 +6,12 @@ namespace exercise_08
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+            Console.WriteLine("What is your name?");
+            string message = Console.ReadLine();
+            Console.WriteLine("Hello " + message + "!");
 
+
+            
         }
     }
 }
