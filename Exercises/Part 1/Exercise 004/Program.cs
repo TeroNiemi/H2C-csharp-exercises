18da7eca7a302836e1c2ad6d4bc5e248b4305ff1
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-02 15:42:20 +0200

    2.11.2020

--- Exercises/Part 1/Exercise 004/Program.cs
+++ Exercises/Part 1/Exercise 004/Program.cs
@@ -6,7 +6,7 @@ namespace exercise_04
     {
         public static void Main(string[] args)
         {
-            string message = "Passport and floss!";
+            string message = "Passport and a toothbrush!";
             Console.WriteLine(message);
         }
     }
