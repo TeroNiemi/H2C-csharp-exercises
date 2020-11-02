d47fb2da1a767710446771b4ad361919e0d6f079
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-02 15:18:15 +0200

    2.11.2020

--- Exercises/Part 1/Exercise 002/Program.cs
+++ Exercises/Part 1/Exercise 002/Program.cs
@@ -6,7 +6,9 @@ namespace exercise_02
     {
         public static void Main(string[] args)
         {
-            // Your code here:
+            Console.WriteLine("Once upon a time");
+            Console.WriteLine("I was falling in love");
+            Console.WriteLine("Now I'm only falling apart");
         }
     }
     
