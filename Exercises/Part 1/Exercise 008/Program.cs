7b86d59ad7dfc5c1d3e8e597364619106f2f0cf1
Ada Udoji <ada.udoji@centria.fi>
2020-11-02 03:15:21 +0200

    2.11.20

--- Exercises/Part 1/Exercise 008/Program.cs
+++ Exercises/Part 1/Exercise 008/Program.cs
@@ -6,7 +6,12 @@ namespace exercise_08
     {
         public static void Main(string[] args)
         {
-            // Write your code here:
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello  "+name+"!");
+
+                Console.ReadLine();
+
 
         }
     }
