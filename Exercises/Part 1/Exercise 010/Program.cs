7073687109ebf244425f779730c45413e637db33
Ada Udoji <ada.udoji@centria.fi>
2020-11-03 09:43:04 +0200

    3.11.2020

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -7,8 +7,20 @@ namespace exercise_10
         public static void Main(string[] args)
         {
           
+            Console.Write("I will tell a story, but I need some information.\nWhat is your name?");
+            string characterName = Console.ReadLine();
 
-           Console.Write(kdfkfj);
+            Console.Write("What is your profession?");
+            string characterProfession = Console.ReadLine();
+
+
+            Console.WriteLine("Here is the story:");
+            Console.WriteLine("Once upon a time there was a "+ characterProfession + " called " + characterName);
+            Console.WriteLine("On her way to work, " + characterName + " often pondered what being " + characterProfession + " meant to them.");
+            Console.WriteLine("When you work as a " + characterProfession + " you meet interesting people.");          
+            Console.WriteLine(""+ characterName + " enjoys their work as " + characterProfession);
+
+           Console.ReadKey();
 
 
 
