765e1b872eac83fcbfcaa8965fff5750ac6f9ac7
Ada Udoji <ada.udoji@centria.fi>
2020-11-04 23:57:44 +0200

    4.11.2020

--- Exercises/Part 1/Exercise 027/Program.cs
+++ Exercises/Part 1/Exercise 027/Program.cs
@@ -6,7 +6,20 @@ namespace exercise_27
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("Give a number:");
+       int number = Convert.ToInt32(Console.ReadLine());
+            
+
+        if (number > 0)
+       {
+        Console.WriteLine("It is positive");
+        }
+        else if (number < 0)
+       {
+       Console.WriteLine("It is not positive");
+        }
+      Console.ReadKey();
+
 
     }
   }
