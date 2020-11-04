4b7d31832694c1eeb4ddbe401bf2b7768841ba78
Ada Udoji <ada.udoji@centria.fi>
2020-11-04 17:53:35 +0200

    4.11.2020

--- Exercises/Part 1/Exercise 013/Program.cs
+++ Exercises/Part 1/Exercise 013/Program.cs
@@ -6,12 +6,13 @@ namespace exercise_13
   {
     public static void Main(string[] args)
     {
-      Console.WriteLine
-                ("Give a number!");
-            double number = 11.11;
-                 Console.WriteLine("You gave " + number); 
+      Console.WriteLine("Give a number!");
+          double number = 11.11;
+      Console.WriteLine("You gave " + number); 
       
-      
+      Console.WriteLine("Give a number!");
+          number = 41.999999;
+      Console.WriteLine("You gave " + number); 
 
       Console.ReadLine();
 
