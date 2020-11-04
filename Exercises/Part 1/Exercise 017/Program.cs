b76e172bf8a7ade7d74565195da78c160beb7a5e
Ada Udoji <ada.udoji@centria.fi>
2020-11-04 21:40:40 +0200

    4.11.2020

--- Exercises/Part 1/Exercise 017/Program.cs
+++ Exercises/Part 1/Exercise 017/Program.cs
@@ -7,8 +7,21 @@ namespace exercise_17
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      
+       Console.WriteLine("Give the first number!");
+       int num1 = Convert.ToInt32(Console.ReadLine());
+      
+       Console.WriteLine("Give the second number!");
+       int num2 = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("The sum is: " + (num1 + num2));
+    
+
+
+
+
+
+      Console.ReadKey();
     }
   }
 }
