dd2580ac81e12c61667d4122b8426bb6c41f13dd
Ada Udoji <ada.udoji@centria.fi>
2020-11-05 00:35:18 +0200

    4.11.2020

--- Exercises/Part 1/Exercise 029/Program.cs
+++ Exercises/Part 1/Exercise 029/Program.cs
@@ -6,7 +6,19 @@ namespace exercise_29
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+    Console.WriteLine("Give the first number!");
+       int num1 = Convert.ToInt32(Console.ReadLine());
+            
+    Console.WriteLine("Give the second number!");
+       int num2 = Convert.ToInt32(Console.ReadLine());
+
+         if (num1 > num2) {
+        Console.WriteLine("The larger number is:" + num1);
+         }
+        else if (num1 < num2) {
+        Console.WriteLine("The larger number is:" + num2);
+        }
+      Console.ReadKey();
 
     }
   }
