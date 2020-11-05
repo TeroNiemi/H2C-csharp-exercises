e5fb66da222819f860214ee645d04b74e5167530
Ada Udoji <ada.udoji@centria.fi>
2020-11-05 12:09:06 +0200

    5.11.2020

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -6,8 +6,32 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("Give your percent [0 - 100]:");
+       int percent = Convert.ToInt32(Console.ReadLine());
 
+            if (percent < 0) {
+        Console.WriteLine("Impossible");
+         }
+            else if (percent <= 49){
+            Console.WriteLine("Fail");
+            }
+            else if (percent <= 59){
+            Console.WriteLine("1");
+            }
+            else if (percent <= 69){
+            Console.WriteLine("2");
+            }
+            else if (percent <= 79){
+            Console.WriteLine("3");
+            }
+            else if (percent <= 89){
+            Console.WriteLine("4");
+            }
+            else if (percent <= 100){
+            Console.WriteLine("Outstanding!");
+            }
+
+        Console.ReadKey();
     }
   }
 }
