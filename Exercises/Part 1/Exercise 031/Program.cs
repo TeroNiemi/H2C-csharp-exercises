f7fc6ca0932987e1515dcdad8b7c7a39d3e86e6c
Ada Udoji <ada.udoji@centria.fi>
2020-11-05 12:57:26 +0200

    5.11.2020

--- Exercises/Part 1/Exercise 031/Program.cs
+++ Exercises/Part 1/Exercise 031/Program.cs
@@ -6,8 +6,22 @@ namespace exercise_31
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      Console.WriteLine("Give a number:");
+       int number = Convert.ToInt32(Console.ReadLine());
+       int remainder = number % 2;
 
+        
+
+            if (remainder ==0) {
+        Console.WriteLine("It is even.");
+        }
+
+            else {
+        Console.WriteLine("It is odd.");
+        }
+        
+
+       Console.ReadKey();
     }
   }
 }
