09b75224f0d924105be2a387e4669f69804987d6
Ada Udoji <ada.udoji@centria.fi>
2020-11-08 22:55:47 +0200

    8.11.2020

--- Exercises/Part 2/Exercise 042/Program.cs
+++ Exercises/Part 2/Exercise 042/Program.cs
@@ -8,7 +8,21 @@ namespace exercise_42
     {
 
       // Write your code here:
-    
+
+          
+            Console.WriteLine("Give two numbers and calculate the square root of the numbers");
+            int first = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+            int sum = first + second;
+            double squareRoot = Math.Sqrt(sum);
+            Console.WriteLine("The sum of " + first + " and " + second + " is " + sum);
+            Console.WriteLine("The squareroot of is:" + squareRoot);
+            
+          
+            
+            
+            
+            Console.ReadKey();
     }
   }
 }
