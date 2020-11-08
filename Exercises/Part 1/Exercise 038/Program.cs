055b59c549c6481d37b9a56acf5c2cb4e25bc54e
Ada Udoji <ada.udoji@centria.fi>
2020-11-08 11:54:03 +0200

    8.11.2020

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,7 +6,25 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+      
+      int neg = 0;
+       while (true) 
+       {
+          Console.WriteLine("Input an integer, 0 quits");
+          int number = Convert.ToInt32(Console.ReadLine());
+             if (number == 0)
+            {
+                break;
+            }
+             else if (number< 0)
+             {
+                neg++;
+             }
+             
+             Console.WriteLine("Total amount of negative numbers: " + neg + "." );
+       }
+
+
 
     }
   }
