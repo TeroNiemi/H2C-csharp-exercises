ad3ec3ab3ef8cf428a01fd20397e3c59d9b45b94
Ada Udoji <ada.udoji@centria.fi>
2020-11-08 12:31:02 +0200

    8.11.2020

--- Exercises/Part 1/Exercise 040/Program.cs
+++ Exercises/Part 1/Exercise 040/Program.cs
@@ -7,7 +7,26 @@ namespace exercise_40
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      int sum = 0;
+      int attempts = 0;
+         while (true)
+         {
+            Console.WriteLine("Input an integer, 0 quits");
+            int number = Convert.ToInt32(Console.ReadLine());
+                if (number == 0)
+                {
+                break;
+                }
+                else
+                {
+                    attempts++;
+                    sum = sum + number;
+
+                }
+                  Console.WriteLine("Total amount of attempts is: " + attempts); 
+                   Console.WriteLine("Total sum of numbers : " + sum);
+         }
+
     }
   }
 }
