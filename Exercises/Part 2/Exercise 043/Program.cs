118c1a2bb68ed3385ef46d96214d2a44c8609471
Ada Udoji <ada.udoji@centria.fi>
2020-11-09 23:05:18 +0200

    9.11.2020

--- Exercises/Part 2/Exercise 043/Program.cs
+++ Exercises/Part 2/Exercise 043/Program.cs
@@ -6,8 +6,30 @@ namespace exercise_43
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
-      
+
+       while (true)
+       {
+        Console.WriteLine("Give an integer and calculate");
+        int first = Convert.ToInt32(Console.ReadLine());
+        int second = -Math.Abs(1);
+       
+         if (first > 0)
+         {
+           
+            Console.WriteLine("The number is : " + first);
+          }
+          if (first < 0)
+          {
+              int product = first * second;
+            Console.WriteLine("The product of " + first + " and " + second + " is " + product);
+          }
+          else if (first == 0)
+                {
+                break;
+                }
+       } 
+           
     }
+
   }
 }
