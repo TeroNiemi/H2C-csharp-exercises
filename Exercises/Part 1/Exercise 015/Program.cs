2b8251a6625d4da512ae76a21992b715b59b7cf8
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-09 13:52:23 +0200

    9.11.2020

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -6,8 +6,24 @@ namespace exercise_15
   {
     public static void Main(string[] args)
     {
-
-      // Write your code here:
+        Console.WriteLine("Give a string:");
+        string UserInput1 = Console.ReadLine();
+        Console.WriteLine("Give an integer:");
+        string UserInput2 = Console.ReadLine();
+        int intValue = Convert.ToInt32(UserInput2);
+            //Tähän asti toimii
+        Console.WriteLine("Give a double: ");
+        string UserInput3 = Console.ReadLine();
+        double doubleValue = Convert.ToDouble(UserInput3);
+            //Toimii
+        Console.WriteLine("Give a boolean: ");
+        string UserInput4 = Console.ReadLine();
+        bool booleanValue = System.Convert.ToBoolean(UserInput4);
+            //Toimii
+        Console.WriteLine("Your string: " + UserInput1);
+        Console.WriteLine("Your integer: " + UserInput2);
+        Console.WriteLine("Your double: " + UserInput3);
+        Console.WriteLine("Your boolean: " + booleanValue);
 
     }
   }
