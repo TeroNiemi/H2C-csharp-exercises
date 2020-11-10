4718ee7a45cd27ff744cf13ee99dd5ecd6f1a82e
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-10 13:58:04 +0200

    10.11.2020

--- Exercises/Part 1/Exercise 021/Program.cs
+++ Exercises/Part 1/Exercise 021/Program.cs
@@ -7,7 +7,11 @@ namespace exercise_21
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give the first number!");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give the second number!");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The average is " + ((double)( num1 + num2) /2));
 
     }
   }
