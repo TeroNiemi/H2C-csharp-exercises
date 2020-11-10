01e1996e239d7f196b3e6682460960391dd8625c
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-10 13:05:57 +0200

    10.11.2020

--- Exercises/Part 1/Exercise 019/Program.cs
+++ Exercises/Part 1/Exercise 019/Program.cs
@@ -7,7 +7,14 @@ namespace exercise_19
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      Console.WriteLine("Give the first number!");
+      int num1 = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("Give the second number!");
+      int num2 = Convert.ToInt32(Console.ReadLine());
+      int sum = num1 + num2;
+      Console.Write(num1 + " + ");
+      Console.Write(num2);
+      Console.Write(" = " + sum + "\n" );
 
     }
   }
