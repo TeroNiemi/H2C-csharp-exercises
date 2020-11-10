2e10ef51aa8d474f1a9090fecc1dd1cb74a963fb
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-10 15:32:52 +0200

    10.11.2020

--- Exercises/Part 1/Exercise 024/Program.cs
+++ Exercises/Part 1/Exercise 024/Program.cs
@@ -7,8 +7,12 @@ namespace exercise_24
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-      
+            Console.WriteLine("Your speed:");
+            int speed = Convert.ToInt32(Console.ReadLine());
+            if (speed > 120)
+            {
+                Console.WriteLine("Speeding!");
+            }
     }
   }
 }
