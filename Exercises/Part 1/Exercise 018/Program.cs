86fe1a9f5b9d180300d087f5cbc608271f54a09f
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-10 11:09:08 +0200

    10.11.2020

--- Exercises/Part 1/Exercise 018/Program.cs
+++ Exercises/Part 1/Exercise 018/Program.cs
@@ -7,7 +7,14 @@ namespace exercise_18
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+     Console.WriteLine("Give the first number!");
+     var num1 = Convert.ToInt32(Console.ReadLine());
+     Console.WriteLine("Give the second number!");
+     var num2 = Convert.ToInt32(Console.ReadLine());
+     Console.WriteLine("Give the third number!");
+     var num3 = Convert.ToInt32(Console.ReadLine());
+     int sum = num1 + num2 + num3;
+     Console.WriteLine("The sum is " + sum );
 
     }
   }
