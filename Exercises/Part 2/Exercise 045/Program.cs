ce0b57d8c1c03f0d772312b03c3b83565227014b
Ada Udoji <ada.udoji@centria.fi>
2020-11-13 19:41:38 +0200

    13.11.2020

--- Exercises/Part 2/Exercise 045/Program.cs
+++ Exercises/Part 2/Exercise 045/Program.cs
@@ -8,6 +8,13 @@ namespace exercise_45
     {
 
       // Write your code here:
+      Console.WriteLine("Please give an integer");
+           int num = Convert.ToInt32(Console.ReadLine());
+            while(num<6)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
 
     }
   }
