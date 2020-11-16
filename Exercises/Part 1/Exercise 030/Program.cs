bdfd2f86eeccdc44ba90f3e6e6b1751b1e661d33
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-16 16:37:18 +0200

    16.11.2020

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -6,9 +6,47 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
-            Console.WriteLine("Give your percent");
-            int perc = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give your percent [0-100]                  ");
+            int percent = Convert.ToInt32(Console.ReadLine());
+            if (percent < 0)
+            {
+            Console.WriteLine("Impossible");
+            }
 
+            else if (percent <= 49)
+            {
+            Console.WriteLine("Fail");
+            }
+
+            else if (percent < 59)
+            {
+            Console.WriteLine("Grade: 1");
+            }
+
+            else if (percent < 69)
+            {
+            Console.WriteLine("Grade: 2");
+            }
+
+            else if (percent < 79)
+            {
+            Console.WriteLine("Grade: 3");
+            }
+
+            else if (percent < 89)
+            {
+            Console.WriteLine("Grade: 4");
+            }
+
+            else if (percent <= 100)
+            {
+            Console.WriteLine("Grade : 5");
+            }
+
+            else if (percent > 100)
+            {
+            Console.WriteLine("Outstanding!");
+            }
     }
   }
 }
