bb71f4dada4914fe44adc30d6403edbd6e3200cc
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-16 17:13:59 +0200

    16.11.2020

--- Exercises/Part 1/Exercise 033/Program.cs
+++ Exercises/Part 1/Exercise 033/Program.cs
@@ -6,8 +6,19 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.WriteLine("Give the first string: ");
+            string first = Console.ReadLine();
+            Console.WriteLine("Give the second string: ");
+            string second = Console.ReadLine();
+            if (first == second)
+            {
+            Console.WriteLine("Echo");
+            }
 
+            else
+            {
+            Console.WriteLine("Nope");
+            }
     }
   }
 }
