a585be5efcadaebc8e3b5cd1874ab4ec9af5e73e
Ada Udoji <ada.udoji@centria.fi>
2020-11-19 18:58:23 +0200

    19.11.2020

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -8,11 +8,40 @@ namespace exercise_48
     {
 
       // Write your code here:
-       Console.WriteLine("Give numbers:");
-       int numb = Convert.ToInt32(Console.ReadLine());
+      int even = 0;
+       int odd = 0; 
+      int remainder;
+      double avrg = 0;
+      int sum = 0;
+       int attempt = 0;
+      Console.WriteLine("Please give numbers");
+        while (true) 
+        {       
+                
+             int input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                     Console.WriteLine("Thx! Bye!");
+                  
+                    break;
+                }
+                    attempt++;
+                    sum += input;  
+                    avrg= sum/attempt;
+                remainder = input % 2;
+                if(remainder== 0)
+                {       
+                    even +=1;
+                   odd += 1;
+                }
+        }  
+             
+             Console.WriteLine("Total sum of numbers is : " + sum);
+             Console.WriteLine("Total sum of attempt is : " + attempt);
+            Console.WriteLine("The average of numbers is : " + avrg);
+            Console.WriteLine("Total amount of even numbers is : " + even);
+            Console.WriteLine("Total amount of odd numbers is : " + odd);
             
-        
-
     }
        
   }
