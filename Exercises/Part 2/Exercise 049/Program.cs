7ec81b7931c9cbe0eaa944b000ff5ffca59245e8
Ada Udoji <ada.udoji@centria.fi>
2020-11-22 22:46:10 +0200

    22.11.2020

--- Exercises/Part 2/Exercise 049/Program.cs
+++ Exercises/Part 2/Exercise 049/Program.cs
@@ -6,15 +6,26 @@ namespace exercise_49
   {
     public static void Main(string[] args)
     {
+          
       // Call your method here:
-      PrintPhrase();
- 
+          
+         PrintPhrase();
+      
     }
 
     // Write your method here:
-    public static void PrintPhrase() 
-    {
-       Console.WriteLine("In a hole in the ground there lived a method");
-    }
+     public static void PrintPhrase()
+     {
+        Console.WriteLine("In a hole in the ground there lived a method");
+      
+     }  
+     
+     
+      
+        
+    
   }
 }
+
+
+
--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -6,13 +6,33 @@ namespace exercise_50
   {
     public static void Main(string[] args)
     {
+          
       // Call your method here:
- 
+       PrintPhrase();
+        
+           
+                
+
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
-
-    }
+     public static void PrintPhrase (int notimes)
+     {
+         int a = 0;  
+        Console.WriteLine("How many times to print?");
+           int notimes = Convert.ToInt32(Console.ReadLine());
+             while (a < notimes)
+            {
+                 Console.WriteLine("In a hole in the ground there lived a method");
+                a++;
+            }
+       
+      
+      }  
+     
+     
+      
+        
+    
   }
 }
