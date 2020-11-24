e9af0e83ed683eae5081c21f97da67dab4f0bf91
Ada Udoji <ada.udoji@centria.fi>
2020-11-24 16:36:17 +0200

    24.11.2020

--- Exercises/Part 2/Exercise 051/Program.cs
+++ Exercises/Part 2/Exercise 051/Program.cs
@@ -7,11 +7,23 @@ namespace exercise_51
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      PrintUntilNumber(5);
 
     }
 
     // Write your method here:
-
+     public static void PrintUntilNumber(int number)
+      {        int a = 1;
+          
+        Console.WriteLine("Give number");
+        
+            while (number < 5)
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                   
+                Console.WriteLine(number);
+                  a++;
+     }       }
+          
   }
 }
