5493464dfaa6652a687e6385ddd834e6f0463aff
Ada Udoji <ada.udoji@centria.fi>
2020-11-24 19:13:37 +0200

    24.11.2020

--- Exercises/Part 2/Exercise 052/Program.cs
+++ Exercises/Part 2/Exercise 052/Program.cs
@@ -7,10 +7,19 @@ namespace exercise_52
     public static void Main(String[] args)
     {
       // Call your method here:
+      PrintFromNumberToOne(5);
 
     }
 
     // Write your method here:
+     public static void PrintFromNumberToOne(int number)
+    {
+        Console.WriteLine("Give number ot times");
+          for (int i= 5; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
 
+     }
   }
 }
