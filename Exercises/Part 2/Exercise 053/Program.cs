f11abf012eaea610d25515f2bb2e722fa89c84ab
Ada Udoji <ada.udoji@centria.fi>
2020-11-25 20:12:42 +0200

    25112020

--- Exercises/Part 2/Exercise 053/Program.cs
+++ Exercises/Part 2/Exercise 053/Program.cs
@@ -7,11 +7,16 @@ namespace exercise_53
     public static void Main(String[] args)
     {
       // Call your method here:
-
+         Division(13, 2);
+            Console.ReadKey();
 
     }
 
     // Write your method here:
-
+     public static void Division(double numerator, double denominator)
+     { 
+      
+        Console.WriteLine(numerator /denominator);
+     }
   }
 }
