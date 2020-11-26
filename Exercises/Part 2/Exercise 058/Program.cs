b4d830beb3b43e607971e840d5b3de588b3bde64
Ada Udoji <ada.udoji@centria.fi>
2020-11-26 23:02:33 +0200

    26112020

--- Exercises/Part 2/Exercise 058/Program.cs
+++ Exercises/Part 2/Exercise 058/Program.cs
@@ -11,6 +11,24 @@ namespace exercise_58
     }
 
     // Write your method here:
+    public static int Smallest(int number1, int number2)
+        {
+            int greater = 0;
+            int Smallest = 0;
+            if (number1 < number2)
+            {
+               Smallest = number1;
+                greater = number2;
+            }else if (number1 > number2)
+            {
+                Smallest = number2;
+                 greater = number1;
+            } else 
+            {
+                Console.WriteLine("same");
+            }
+            return Smallest;
+        }
     
   }
 }
